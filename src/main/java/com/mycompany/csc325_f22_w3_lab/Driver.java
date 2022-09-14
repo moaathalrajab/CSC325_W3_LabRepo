0f36d9d8286103493230d5ed690ebd0d1d1bfa5d
soblab <soblab@WHIT208W10_00.it.farmingdale.edu>
2022-09-14 10:13:58 -0400

    Add new object inside main and print

--- src/main/java/com/mycompany/csc325_f22_w3_lab/Driver.java
+++ src/main/java/com/mycompany/csc325_f22_w3_lab/Driver.java
@@ -5,6 +5,9 @@ package com.mycompany.csc325_f22_w3_lab;
 public class Driver {
     
     public static void main(String[] args) {
+        Student std = new Student();
+        
+        System.out.println("Moath Alrajab");
         
     }
     
