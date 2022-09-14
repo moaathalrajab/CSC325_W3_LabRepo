d647672802b7ae834c860beb4b27e6986308b702
soblab <soblab@WHIT208W10_00.it.farmingdale.edu>
2022-09-14 10:03:07 -0400

    Add setter and getter to the Student class

--- src/main/java/com/mycompany/csc325_f22_w3_lab/Student.java
+++ src/main/java/com/mycompany/csc325_f22_w3_lab/Student.java
@@ -6,6 +6,22 @@ public class Student {
     String fName;
     String lName;
     double gpa;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
     
     
 }
